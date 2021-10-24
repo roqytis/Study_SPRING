1fddccb81b6ef5441ae5450f9c51a3513af3dddf
roqytis <91776641+roqytis@users.noreply.github.com>
2021-10-24 16:56:12 +0900

    06Day8_MVC8_config_initParam3

---
+++ 06Day8_MVC8_config_initParam3/src/main/java/com/controller/TestController.java
@@ -0,0 +1,38 @@
+package com.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+	@Autowired
+	TestService service;
+	@Autowired
+	Person person;
+	//////////////////////////////////////
+	@RequestMapping(value = "/xxx")
+	public String sayEcho() {
+		System.out.println(service.xx()+"\t"+person.toString());
+		return "login";
+	}
+	/////////////////////////////////
+	
+
+	public TestService getService() {
+		return service;
+	}
+
+	public void setService(TestService service) {
+		this.service = service;
+	}
+
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+	
+}
---
+++ 06Day8_MVC8_config_initParam3/src/main/java/com/controller/TestService.java
@@ -0,0 +1,7 @@
+package com.controller;
+
+public class TestService {
+    public String xx() {
+    	return "TestService.xx() 함수 실해됨";
+    }
+}
