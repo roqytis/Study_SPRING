9d7176e752adbb8786948c0a247ebcf22a593416
roqytis <91776641+roqytis@users.noreply.github.com>
2021-10-24 14:17:37 +0900

    01Day5_import

---
+++ 01Day5_import/src/main/java/PersonTest.java
@@ -0,0 +1,33 @@
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import com.spring.Animal;
+import com.spring.Person;
+
+public class PersonTest {
+
+	public static void main(String[] args) {
+		
+		
+		GenericXmlApplicationContext ctx= new GenericXmlApplicationContext("com/kkk/person.xml");
+		
+		Person p1= ctx.getBean("xxx", Person.class);
+		System.out.println(p1.getInfo());
+		
+		Animal p2=(Animal)ctx.getBean("yyy");
+		System.out.println(p2.getName());
+		//GenericXmlApplicationContext ctx= new GenericXmlApplicationContext("com/kkk/person.xml");
+		
+		
+		//Person p1= ctx.getBean("xxx", Person.class);
+		//System.out.println(p1.getInfo());
+		//Person p2=(Person)ctx.getBean("xxx");
+		//System.out.println(p2.getInfo());
+	  
+		
+		//Animal a1= ctx.getBean("yyy",Animal.class);
+	    //System.out.println(a1.getName());
+	    //Animal a2=(Animal)ctx.getBean("yyy");
+	    //System.out.println(a2.getName());
+	}
+
+}
---
+++ 01Day5_import/src/main/java/com/spring/Person.java
@@ -0,0 +1,12 @@
+package com.spring;
+
+public class Person {
+
+	public Person() {
+		super();
+		System.out.println("생성자 호출");
+	}
+ public String getInfo() {
+	 return "이순신";
+ }
+}
