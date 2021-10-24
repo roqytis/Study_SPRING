c9290f0a4baff18db4dd15505d151ecf1a2cb43e
roqytis <91776641+roqytis@users.noreply.github.com>
2021-10-24 14:32:47 +0900

    02Day_BeanTest3_injection2_setter(autowire_byType_Error)

---
+++ 02Day_BeanTest3_injection2_setter(autowire_byType_Error)/src/main/java/com/spring/PersonTest.java
@@ -0,0 +1,17 @@
+package com.spring;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class PersonTest {
+
+	public static void main(String[] args) {
+		
+		GenericXmlApplicationContext ctx=new GenericXmlApplicationContext("person.xml");
+		
+		Person p1=ctx.getBean("onePerson", Person.class);
+		//Person p2=ctx.getBean("onePerson", Person.class);
+		//System.out.println(p1==p2);
+		System.out.println(p1.getCat()+"\t"+p1.getDog());
+	}
+
+}
