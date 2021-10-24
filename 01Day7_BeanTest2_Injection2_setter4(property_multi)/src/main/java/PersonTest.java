9b4b4cf9a4a7634f2f6c42b9109a330d4850ca6d
roqytis <91776641+roqytis@users.noreply.github.com>
2021-10-24 14:24:35 +0900

    01Day7_BeanTest2_Injection2_setter4(property_multi)

---
+++ 01Day7_BeanTest2_Injection2_setter4(property_multi)/src/main/java/PersonTest.java
@@ -0,0 +1,16 @@
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import com.spring.Person;
+
+public class PersonTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	       GenericXmlApplicationContext ctx= new GenericXmlApplicationContext("person.xml");
+	       
+	       Person p= ctx.getBean("one",Person.class);
+	       
+	       System.out.println(p.getCat().getCatName()+"\t"+p.getCat().getCatAge());
+	       System.out.println(p.getDog().getDogName()+"\t"+p.getDog().getDogAge());
+		}
+}
