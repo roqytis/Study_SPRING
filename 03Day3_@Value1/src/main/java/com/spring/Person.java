abec43b3d856189819d3a8df6e439786d5c6fe1d
roqytis <91776641+roqytis@users.noreply.github.com>
2021-10-24 14:55:01 +0900

    03Day3_@Value1

---
+++ 03Day3_@Value1/src/main/java/com/spring/Person.java
@@ -0,0 +1,36 @@
+package com.spring;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class Person {
+    @Value("홍길동")
+	String username;
+	@Value("10")
+	int age;
+	public Person() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Person(String username, int age) {
+		super();
+		this.username = username;
+		this.age = age;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "Person [username=" + username + ", age=" + age + "]";
+	}
+	
+}
