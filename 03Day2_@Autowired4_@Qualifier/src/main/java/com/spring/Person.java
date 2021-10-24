f223e1b2fa51f428a367841f9a57ce72ba3600b6
roqytis <91776641+roqytis@users.noreply.github.com>
2021-10-24 14:46:25 +0900

    03Day2_@Autowired4_@Qualifier

---
+++ 03Day2_@Autowired4_@Qualifier/src/main/java/com/spring/Person.java
@@ -0,0 +1,50 @@
+package com.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class Person {
+
+	String username;
+	int age;
+	//***************************************************************//
+	@Autowired(required = false) //자동주입 byType와 동일
+	@Qualifier("pet01") //특정 bean id지정
+    Cat cat;
+  //***************************************************************//
+    
+	public Person() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Person(String username, int age, Cat cat) {
+		super();
+		this.username = username;
+		this.age = age;
+		this.cat = cat;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Cat getCat() {
+		return cat;
+	}
+	public void setCat(Cat cat) {
+	 System.out.println("Person.setCat함수호출()=============");
+		this.cat = cat;
+	}
+	@Override
+	public String toString() {
+		return "Pesron [username=" + username + ", age=" + age + ", cat=" + cat + "]";
+	}
+    
+}
