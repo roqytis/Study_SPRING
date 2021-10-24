d5b99e2f49e7ad35540270bd3da65b1bf345a292
roqytis <91776641+roqytis@users.noreply.github.com>
2021-10-24 14:34:17 +0900

    02Day_BeanTest5_injection2_setter(muti_config)

---
+++ 02Day_BeanTest5_injection2_setter(muti_config)/src/main/java/com/dto/Person.java
@@ -0,0 +1,42 @@
+package com.dto;
+
+public class Person {
+
+	private String usename;
+	Cat cat;
+	Dog dog;
+	public Person() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Person(String usename, Cat cat, Dog dog) {
+		super();
+		this.usename = usename;
+		this.cat = cat;
+		this.dog = dog;
+	}
+	public String getUsename() {
+		return usename;
+	}
+	public void setUsename(String usename) {
+		this.usename = usename;
+	}
+	public Cat getCat() {
+		return cat;
+	}
+	public void setCat(Cat cat) {
+		System.out.println("set============");
+		this.cat = cat;
+	}
+	public Dog getDog() {
+		return dog;
+	}
+	public void setDog(Dog dog) {
+		this.dog = dog;
+	}
+	@Override
+	public String toString() {
+		return "Person [usename=" + usename + ", cat=" + cat + ", dog=" + dog + "]";
+	}
+	
+}
