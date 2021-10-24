a2376ef5281732745e8f8b44a6ca725ba2b582d6
roqytis <91776641+roqytis@users.noreply.github.com>
2021-10-24 14:21:42 +0900

    01Day6_constructor_injection5_Dept_setter

---
+++ 01Day6_constructor_injection5_Dept_setter/src/main/java/com/dao/DeptDAO.java
@@ -0,0 +1,12 @@
+package com.dao;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DeptDAO {
+  
+	 public List<String> getList(){
+	   return Arrays.asList("A","B","C");	 
+	 
+ }
+}
