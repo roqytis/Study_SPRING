cf2f3b3fa16487ae1f4697331c0156417b1eda9f
roqytis <91776641+roqytis@users.noreply.github.com>
2021-10-24 14:25:48 +0900

    01Day7_BeanTest2_Injection2_setter5(property_primtive)

---
+++ 01Day7_BeanTest2_Injection2_setter5(property_primtive)/src/main/java/com/spring/Person.java
@@ -0,0 +1,52 @@
+package com.spring;
+
+public class Person {
+
+	String username;
+	int age;
+	Cat cat;
+	Dog dog;
+	@Override
+	public String toString() {
+		return "Person [username=" + username + ", age=" + age + ", cat=" + cat + ", dog=" + dog + "]";
+	}
+	public Person() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Person(String username, int age, Cat cat, Dog dog) {
+		super();
+		this.username = username;
+		this.age = age;
+		this.cat = cat;
+		this.dog = dog;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Cat getCat() {
+		return cat;
+	}
+	public void setCat(Cat cat) {
+		this.cat = cat;
+	}
+	public Dog getDog() {
+		return dog;
+	}
+	public void setDog(Dog dog) {
+		this.dog = dog;
+	}
+	
+	
+	
+	
+}
