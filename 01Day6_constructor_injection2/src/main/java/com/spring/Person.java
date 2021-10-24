248dd6f6293d6d17f7832f6d6af590b4a6c30e69
roqytis <91776641+roqytis@users.noreply.github.com>
2021-10-24 14:18:43 +0900

    01Day6_constructor_injection2

---
+++ 01Day6_constructor_injection2/src/main/java/com/spring/Person.java
@@ -0,0 +1,21 @@
+package com.spring;
+
+public class Person {
+
+	public Person(String x) {
+		super();
+		System.out.println("person 생성자 호출"+x);
+	}
+	public Person(String x, String y) {
+		super();
+		System.out.println("person 생성자 호출 x,y"+x+"\t"+y);
+	}
+	public Person(String xx, int yy) {
+		super();
+		System.out.println("person 생성자 호출 xx,yy"+xx+"\t"+yy);
+	}
+	public String getInfo() {
+		return "홍길동";
+	}
+
+}
