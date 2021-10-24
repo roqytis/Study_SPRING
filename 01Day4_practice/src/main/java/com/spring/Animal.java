7e2b90801a35f6b798c764625ee59c9cd0d2a500
roqytis <91776641+roqytis@users.noreply.github.com>
2021-10-24 14:17:14 +0900

    01Day4_practice

---
+++ 01Day4_practice/src/main/java/com/spring/Animal.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+public class Animal {
+
+	public Animal() {
+		super();
+		System.out.println("Animal 생성자====");
+	}
+    public String getName() {
+    	return "고양이";
+    }
+
+}
---
+++ 01Day4_practice/src/main/java/com/spring/Student.java
@@ -0,0 +1,12 @@
+package com.spring;
+
+public class Student {
+
+	public Student() {
+		super();
+		System.out.println("생성자 호출");
+	}
+	  public String getName() {
+	    	return "홍길동";
+	    }
+}
