17ef5efcc2fe2d9e0db0cb279130824f6f2aff98
roqytis <91776641+roqytis@users.noreply.github.com>
2021-10-24 14:20:27 +0900

    01Day6_constructor_injection4

---
+++ 01Day6_constructor_injection4/src/main/java/Person.java
@@ -0,0 +1,13 @@
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class Person {
+
+	public static void main(String[] args) {
+	
+		GenericXmlApplicationContext ctx= new GenericXmlApplicationContext("com/spring/person.xml");
+		Person xxx = ctx.getBean("xxx",Person.class);
+		System.out.println(xxx.toString());
+     
+	}
+
+}
