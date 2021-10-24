dc11a28230003766bc935185b781e48c84ad1859
roqytis <91776641+roqytis@users.noreply.github.com>
2021-10-24 14:15:49 +0900

    01Day3_file

---
+++ 01Day3_file/src/main/java/PersonTest.java
@@ -0,0 +1,11 @@
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class PersonTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		GenericXmlApplicationContext ctx= new GenericXmlApplicationContext("file:\\C:\\upload\\person.xml");
+	}
+
+}
---
+++ 01Day3_file/src/main/java/com/spring/Person.java
@@ -0,0 +1,16 @@
+package com.spring;
+
+public class Person {
+
+
+
+	public Person() {
+		super();
+		System.out.println("생성자 호출");
+	}
+   
+	public String getInfo() {
+		// TODO Auto-generated method stub
+		return "이순신";
+	}
+}
